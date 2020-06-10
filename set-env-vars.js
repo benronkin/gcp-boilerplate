7e9cf889d9d7a57b03499d4fbe8636e1a06a5bba
Ben Ronkin <ronkinben@gmail.com>
2020-06-10 15:16:09 -0700

    Improved colon split

--- set-env-vars.js
+++ set-env-vars.js
@@ -4,7 +4,7 @@ const [_, __, envFileName, command, scriptName] = process.argv;
 fs.readFile(envFileName, 'utf-8', (err, vars) => {
   if (err) throw err;
   vars.split('\n').forEach((va) => {
-    const [k, v] = va.split(':');
+    const [k, v] = va.split(/:(.+)/);
     if (k) {
       process.env[k.trim()] = v.trim();
     }
