b8849b138d4657bcc954d59778df13570fbd9b7c
Ben Ronkin <ronkinben@gmail.com>
2020-06-10 10:26:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Clone this project as a boilerplate for Google Cloud Platform deployments that involve environment variables.
---
+++ set-env-vars.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const spawn = require('cross-spawn');
+const [_, __, envFileName, command, scriptName] = process.argv;
+fs.readFile(envFileName, 'utf-8', (err, vars) => {
+  if (err) throw err;
+  vars.split('\n').forEach((va) => {
+    const [k, v] = va.split(':');
+    if (k) {
+      process.env[k.trim()] = v.trim();
+    }
+  });
+  spawn(command, [scriptName], {
+    stdio: 'inherit',
+  });
+});
